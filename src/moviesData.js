c03697c10dd57623ab9bae0830ef0c7409cee811
ankit <ankitchakraborty@30@gmail.com>
2024-07-21 17:25:54 +0530

    Resolved merge conflicts

---
+++ src/moviesData.js
@@ -0,0 +1,113 @@
+export const movies = [
+  {
+    title: "The Avengers",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Avatar: The Way of Water",
+    plot: "Avatar: The Way of Water begins to tell the story of the Sully family Jake, Neytiri, and their kids, the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDMrR4Ful0hmgitEEw7Gkz9T_9pz1XInPffA&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Titanic",
+    plot: "James Cameron's Titanic is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the ship of dreams -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgE82y-HjMVZ7QK9OHDykFWn0CgBT_14gtGQ&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Minions",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqDTGJJLrTu8kcW-NMq9BBO7tSufyMlHXl9Q&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Ironman",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiHVb3THoeeGpfQkkWs9YqaUBUKju5TnItSw&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "The chronicles of narnia",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://c8.alamy.com/comp/BY99A0/the-chronicles-of-narnia-the-voyage-of-the-dawn-treader-year-2010-BY99A0.jpg",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Jumanji",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgz_KJGpjgJTsO0RKwnmT3e9FI9lK6kOz8A&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Pirates Of The Caribbean",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7VZXxN_KHJz3J5GgujyIaxrj7ybld6jHqZg&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+
+  {
+    title: "DeadPool",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtUUP-sSohKnGmZHGXdfHx3vVCYXldMMl1bw&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "The Hulk",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyBkT9khJbmtU5uJlvjm6i4pYVcrth1X8mOg&s",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+];
